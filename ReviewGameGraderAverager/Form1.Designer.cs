172395210ebcfa9e70e6bbe0576c07717ca88501
LazyDragonStudios <ainsworth.j.alexis@gmail.com>
2025-03-08 20:30:44 -0500

    Initial commit

---
+++ ReviewGameGraderAverager/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ReviewGameGraderAverager
+{
+    partial class DigitalReviewGameGradeAverager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DragAndDropFilesBox = new System.Windows.Forms.ListBox();
+            this.CreateNewDataButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.DownloadData = new System.Windows.Forms.Button();
+            this.OutputFileName = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numQuestionThreshold = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.contextMenuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(680, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drag and Drop CSV files from Blooket or Gimkit, Ensure students have used the exa" +
+    "ct same username in each file. Letter Case does not matter.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(344, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "EnterThe Desired Name of the output file, ie. Period2Week10WarmUps";
+            // 
+            // DragAndDropFilesBox
+            // 
+            this.DragAndDropFilesBox.AllowDrop = true;
+            this.DragAndDropFilesBox.ContextMenuStrip = this.contextMenuStrip2;
+            this.DragAndDropFilesBox.FormattingEnabled = true;
+            this.DragAndDropFilesBox.Location = new System.Drawing.Point(4, 75);
+            this.DragAndDropFilesBox.Name = "DragAndDropFilesBox";
+            this.DragAndDropFilesBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.DragAndDropFilesBox.Size = new System.Drawing.Size(745, 95);
+            this.DragAndDropFilesBox.TabIndex = 2;
+            this.DragAndDropFilesBox.SelectedIndexChanged += new System.EventHandler(this.DragAndDropFilesBox_SelectedIndexChanged);
+            this.DragAndDropFilesBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.DragAndDropFilesBox_KeyDown_1);
+            // 
+            // CreateNewDataButton
+            // 
+            this.CreateNewDataButton.Location = new System.Drawing.Point(570, 179);
+            this.CreateNewDataButton.Name = "CreateNewDataButton";
+            this.CreateNewDataButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.CreateNewDataButton.Size = new System.Drawing.Size(112, 43);
+            this.CreateNewDataButton.TabIndex = 3;
+            this.CreateNewDataButton.Text = "Create File";
+            this.CreateNewDataButton.UseVisualStyleBackColor = true;
+            this.CreateNewDataButton.Click += new System.EventHandler(this.CreateNewDataButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 255);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(737, 555);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // DownloadData
+            // 
+            this.DownloadData.Location = new System.Drawing.Point(321, 816);
+            this.DownloadData.Name = "DownloadData";
+            this.DownloadData.Size = new System.Drawing.Size(108, 23);
+            this.DownloadData.TabIndex = 5;
+            this.DownloadData.Text = "Download Data";
+            this.DownloadData.UseVisualStyleBackColor = true;
+            this.DownloadData.Click += new System.EventHandler(this.DownloadData_Click);
+            // 
+            // OutputFileName
+            // 
+            this.OutputFileName.Location = new System.Drawing.Point(4, 25);
+            this.OutputFileName.Name = "OutputFileName";
+            this.OutputFileName.Size = new System.Drawing.Size(341, 20);
+            this.OutputFileName.TabIndex = 6;
+            this.OutputFileName.TextChanged += new System.EventHandler(this.OutputFileName_TextChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 192);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numQuestionThreshold
+            // 
+            this.numQuestionThreshold.AutoSize = true;
+            this.numQuestionThreshold.Location = new System.Drawing.Point(9, 173);
+            this.numQuestionThreshold.Name = "numQuestionThreshold";
+            this.numQuestionThreshold.Size = new System.Drawing.Size(487, 13);
+            this.numQuestionThreshold.TabIndex = 8;
+            this.numQuestionThreshold.Text = "Please, Enter the munimum number of questions the student is required to answer t" +
+    "o receive full credit.";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(108, 26);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click_1);
+            // 
+            // DigitalReviewGameGradeAverager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 860);
+            this.Controls.Add(this.numQuestionThreshold);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.OutputFileName);
+            this.Controls.Add(this.DownloadData);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.CreateNewDataButton);
+            this.Controls.Add(this.DragAndDropFilesBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "DigitalReviewGameGradeAverager";
+            this.Text = "Digital Review Game Grade Averager";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox DragAndDropFilesBox;
+        private System.Windows.Forms.Button CreateNewDataButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button DownloadData;
+        private System.Windows.Forms.TextBox OutputFileName;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label numQuestionThreshold;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+    }
+}
+
---
+++ ReviewGameGraderAverager/Form1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ClosedXML.Excel;
+
+
+
+namespace ReviewGameGraderAverager
+{
+    public partial class DigitalReviewGameGradeAverager : Form
+    {
+
+
+
+        public DigitalReviewGameGradeAverager()
+        {
+            InitializeComponent();
+            DragAndDropFilesBox.DragEnter += DragAndDropFilesBox_DragEnter;
+            DragAndDropFilesBox.DragDrop += DragAndDropFilesBox_DragDrop;
+        }
+
+
+
+        private void OutputFileName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DragAndDropFilesBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void DragAndDropFilesBox_DragEnter(object sender, DragEventArgs e)
+        {
+            // Allow file drop
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy; // Allow copy operation
+            }
+        }
+
+        private void DragAndDropFilesBox_DragDrop(object sender, DragEventArgs e)
+        {
+            // Get the dropped files (as an array of strings)
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            // Display the file names in the ListView
+            foreach (string file in files)
+            {
+                // Create a ListViewItem with the file name
+                ListViewItem listItem = new ListViewItem(file);
+
+                // Add the ListViewItem to the ListView control
+                DragAndDropFilesBox.Items.Add(listItem);
+            }
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void CreateNewDataButton_Click(object sender, EventArgs e)
+        {
+            // Process the CSV files and display the data
+            ProcessAndDisplayData();
+        }
+
+
+        private void DownloadData_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV Files (*.csv)|*.csv";
+            saveFileDialog.FileName = OutputFileName.Text;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                // Add the header row
+                foreach (DataGridViewColumn column in dataGridView1.Columns)
+                {
+                    sb.Append(column.HeaderText + ",");
+                }
+                sb.AppendLine();
+
+                // Add the data rows
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        sb.Append(cell.Value + ",");
+                    }
+                    sb.AppendLine();
+                }
+
+                // Write to the file
+                File.WriteAllText(saveFileDialog.FileName, sb.ToString());
+            }
+        }
+
+
+        private List<ScoreData> ReadCsvFile(string filePath, List<string> playerColumnNames, List<string> correctColumnNames, List<string> incorrectColumnNames, int minQuestionsThreshold)
+        {
+            var scoreDataList = new List<ScoreData>();
+
+            try
+            {
+                // Read all lines from the CSV file
+                var lines = File.ReadAllLines(filePath);
+
+                if (lines.Length == 0)
+                    return scoreDataList;
+
+                // Assuming the first line is the header, and subsequent lines are the data
+                var header = lines[0].Split(',');
+
+                int playerColumnIndex = -1;
+                int correctColumnIndex = -1;
+                int incorrectColumnIndex = -1;
+
+                // Find column indices
+                for (int i = 0; i < header.Length; i++)
+                {
+                    var columnName = header[i].Trim().ToLower();
+
+                    if (playerColumnNames.Any(name => name.Trim().ToLower() == columnName))
+                    {
+                        playerColumnIndex = i;
+                    }
+                    if (correctColumnNames.Any(name => name.Trim().ToLower() == columnName))
+                    {
+                        correctColumnIndex = i;
+                    }
+                    if (incorrectColumnNames.Any(name => name.Trim().ToLower() == columnName))
+                    {
+                        incorrectColumnIndex = i;
+                    }
+                }
+
+                if (playerColumnIndex == -1 || correctColumnIndex == -1 || incorrectColumnIndex == -1)
+                {
+                    MessageBox.Show("Unable to find necessary columns in the CSV file.");
+                    return scoreDataList;
+                }
+
+                // Process the remaining lines for score data
+                foreach (var line in lines.Skip(1)) // Skip header row
+                {
+                    var columns = line.Split(',');
+
+                    var playerName = columns[playerColumnIndex].Trim(); // Keep capitalization for display
+                    int correct = int.TryParse(columns[correctColumnIndex].Trim(), out int c) ? c : 0;
+                    int incorrect = int.TryParse(columns[incorrectColumnIndex].Trim(), out int i) ? i : 0;
+
+                    int totalAnswered = correct + incorrect;
+
+                    // Adjust incorrect answers if below threshold
+                    if (totalAnswered < minQuestionsThreshold)
+                    {
+                        incorrect += (minQuestionsThreshold - totalAnswered);
+                        totalAnswered = minQuestionsThreshold;
+                    }
+
+                    decimal accuracy = totalAnswered > 0 ? (correct / (decimal)totalAnswered) * 100 : 0;
+
+                    scoreDataList.Add(new ScoreData { PlayerName = playerName, Score = accuracy });
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error reading file {filePath}: {ex.Message}");
+            }
+
+            return scoreDataList;
+        }
+
+        // New Method to Convert .xlsx to .csv
+        private string ConvertXlsxToCsv(string xlsxFilePath)
+        {
+            string tempCsvFilePath = Path.Combine(Path.GetTempPath(), Path.GetFileNameWithoutExtension(xlsxFilePath) + ".csv");
+
+            try
+            {
+                // Open the Excel file using ClosedXML
+                using (var workbook = new XLWorkbook(xlsxFilePath))
+                {
+                    var worksheet = workbook.Worksheets.Worksheet(1); // Assuming the data is in the first worksheet
+                    var rows = worksheet.RowsUsed(); // Get all used rows
+
+                    // Write the rows to a CSV file
+                    using (var writer = new StreamWriter(tempCsvFilePath))
+                    {
+                        foreach (var row in rows)
+                        {
+                            var cells = row.Cells().Select(cell => cell.Value.ToString());
+                            string line = string.Join(",", cells);
+                            writer.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error converting {xlsxFilePath} to CSV: {ex.Message}");
+            }
+
+            return tempCsvFilePath;
+        }
+
+        private void ProcessAndDisplayData()
+        {
+            // Clear the DataGridView to prevent duplicate data
+            dataGridView1.Rows.Clear();
+
+            var allScoreData = new List<ScoreData>();
+
+
+            var playerColumnNames = new List<string> { "Player Name", "PlayerName", "Name" }; // Add other possibilities
+            var correctColumnNames = new List<string> { "Corrects", "Questions Answered Correctly" }; // Add other possibilities
+            var incorrectColumnNames = new List<string> { "Incorrects", "Questions Answered Incorrectly"}; // Add other possibilities
+                                                                                 
+            int minQuestionsThreshold = (int)numericUpDown1.Value;
+
+            // Loop through each file in the ListView and read data
+            foreach (ListViewItem item in DragAndDropFilesBox.Items)
+            {
+                string filePath = item.Text;
+                if (Path.GetExtension(filePath).ToLower() == ".xlsx")
+                {
+                    // Convert .xlsx to .csv
+                    string tempCsvFilePath = ConvertXlsxToCsv(filePath);
+
+                    // Process the converted CSV file
+                    var scoreDataList = ReadCsvFile(tempCsvFilePath, playerColumnNames, correctColumnNames, incorrectColumnNames, minQuestionsThreshold);
+
+                    // Optionally delete the temporary CSV file after processing
+                    File.Delete(tempCsvFilePath);
+
+                    // Add data from the current file to the list
+                    allScoreData.AddRange(scoreDataList);
+                }
+                else if (Path.GetExtension(filePath).ToLower() == ".csv")
+                {
+                    // Process the CSV file directly
+                    var scoreDataList = ReadCsvFile(filePath, playerColumnNames, correctColumnNames, incorrectColumnNames, minQuestionsThreshold);
+
+                    // Add data from the current file to the list
+                    allScoreData.AddRange(scoreDataList);
+                }
+                else
+                {
+                    MessageBox.Show("Unsupported file format. Only .csv and .xlsx files are supported.");
+                }
+               
+              
+            }
+
+            // Group by player name (case-insensitive and ignore white space)
+            var groupedData = allScoreData
+                .GroupBy(s => s.PlayerName.Trim().ToLower()) // Normalize player name (case-insensitive and trim white space)
+                .Select(g => new
+                {
+                    PlayerName = g.Key,
+                    Scores = g.Select(s => s.Score).ToList()
+                })
+                .Select(g => new
+                {
+                    PlayerName = g.PlayerName,
+                    Average = AverageScores(g.Scores),
+                    Lowest = g.Scores.Min(),
+                    Highest = g.Scores.Max()
+                })
+                .ToList();
+
+            // Create columns in DataGridView if not already created
+            if (dataGridView1.Columns.Count == 0)
+            {
+                dataGridView1.Columns.Add("Player Name", "Player Name");
+                dataGridView1.Columns.Add("Average", "Average");
+                dataGridView1.Columns.Add("Lowest", "Lowest");
+                dataGridView1.Columns.Add("Highest", "Highest");
+            }
+
+            // Display the data in DataGridView
+            foreach (var playerData in groupedData)
+            {
+                dataGridView1.Rows.Add(playerData.PlayerName,
+                                       $"{playerData.Average:F2}%",  // Display Average as formatted percentage
+                                       $"{playerData.Lowest:F2}%",   // Display Lowest as formatted percentage
+                                       $"{playerData.Highest:F2}%"); // Display Highest as formatted percentage
+            }
+
+
+        }
+
+
+
+        private decimal AverageScores(IEnumerable<decimal> scores)
+        {
+            if (!scores.Any())
+                return 0m; // If no valid scores, return 0
+
+            return scores.Average();  // Calculate and return the average of the decimals
+        }
+
+        private void deleteToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            if (DragAndDropFilesBox.SelectedItems.Count > 0)
+            {
+                // Create a temporary list to store selected items
+                var itemsToRemove = DragAndDropFilesBox.SelectedItems.Cast<ListViewItem>().ToList();
+
+                // Now remove items safely outside the loop
+                foreach (var item in itemsToRemove)
+                {
+                    DragAndDropFilesBox.Items.Remove(item);
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DragAndDropFilesBox_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete) // Check if Delete key was pressed
+            {
+                if (DragAndDropFilesBox.SelectedItems.Count > 0)
+                {
+                    // Create a separate list to store selected items
+                    List<ListViewItem> itemsToRemove = new List<ListViewItem>();
+
+                    foreach (ListViewItem item in DragAndDropFilesBox.SelectedItems)
+                    {
+                        itemsToRemove.Add(item); // Store selected items first
+                    }
+
+                    // Now remove them safely
+                    foreach (var item in itemsToRemove)
+                    {
+                        DragAndDropFilesBox.Items.Remove(item);
+                    }
+                }
+            }
+        }
+    }
+    public class ScoreData
+    {
+        public string PlayerName { get; set; }
+        public decimal Score { get; set; }
+    }
+}
